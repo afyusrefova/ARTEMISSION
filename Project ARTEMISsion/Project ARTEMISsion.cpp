144402d74a8b9554aad873a9fcf51214d683cb20
afyusrefova <afyusrefova@codingburgas.bg>
2021-07-05 13:31:14 +0300

    Initial commit

---
+++ Project ARTEMISsion/Project ARTEMISsion.cpp
@@ -0,0 +1,9 @@
+// Project ARTEMISsion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "ARTEMISsion" << endl;
+}
