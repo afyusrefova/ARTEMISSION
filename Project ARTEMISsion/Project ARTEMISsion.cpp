ec41d60ee76af5546c78b200dfc022cd1e77620b
afyusrefova <afyusrefova18@codingburgas.bg>
2021-07-07 09:56:07 +0300

    Add structures, enums and a genderEnumToString function

--- Project ARTEMISsion/Project ARTEMISsion.cpp
+++ Project ARTEMISsion/Project ARTEMISsion.cpp
@@ -1,8 +1,48 @@
-// Project ARTEMISsion.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <string>
 using namespace std;
+enum gender {
+    MALE, FEMALE
+};
+enum condition
+{
+    GOOD, BAD;
+};
+
+struct DATE
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct DOG
+{
+    string name;
+    int id;
+    string breed;
+    gender gender;
+    condition condition;
+    DATE dateOfArrival;
+    DATE dateOfAdoption;
+};
+string genderEnumToString(DOG* dogs, int i)
+{
+    string gender;
+    if (dogs[i].gender = 0)
+    {
+        gender = "Male";
+
+    }
+    else if (dog[i].gender == 1)
+    {
+        gender = "Female";
+    }
+    return gender;
+}
+
 
 int main()
 {
