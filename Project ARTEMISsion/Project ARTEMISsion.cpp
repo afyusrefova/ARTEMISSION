f5f4086fa19b61af5b4f44f91a3c291e93542452
afyusrefova <afyusrefova18@codingburgas.bg>
2021-07-07 11:09:43 +0300

    Add 5-10 functions

--- Project ARTEMISsion/Project ARTEMISsion.cpp
+++ Project ARTEMISsion/Project ARTEMISsion.cpp
@@ -8,7 +8,7 @@ enum gender {
 };
 enum condition
 {
-    GOOD, BAD;
+    GOOD, BAD
 };
 
 struct DATE
@@ -23,6 +23,7 @@ struct DOG
     string name;
     int id;
     string breed;
+    float age;
     gender gender;
     condition condition;
     DATE dateOfArrival;
@@ -31,12 +32,12 @@ struct DOG
 string genderEnumToString(DOG* dogs, int i)
 {
     string gender;
-    if (dogs[i].gender = 0)
+    if (dogs[i].gender == 0)
     {
         gender = "Male";
 
     }
-    else if (dog[i].gender == 1)
+    else if (dogs[i].gender == 1)
     {
         gender = "Female";
     }
@@ -59,13 +60,13 @@ string conditionEnumToString(DOG* dogs, int i)
 
 void showDog(DOG* dogs, int i, string gender, string condition)
 {
-    cout << "Name: " << animals[i].name << endl;
-    cout << "ID: " << animals[i].id << endl;
-    cout << "Breed: " << animals[i].breed << endl;
-    cout << "Age (in human years): " << animals[i].age << endl;
+    cout << "Name: " << dogs[i].name << endl;
+    cout << "ID: " << dogs[i].id << endl;
+    cout << "Breed: " << dogs[i].breed << endl;
+    cout << "Age (in human years): " << dogs[i].age << endl;
     cout << "Gender: " << gender << endl;
     cout << "Condition: " << condition << endl;
-    cout << "Date of arrival: " << animals[i].dateOfArrival.day << "/" << animals[i].dateOfArrival.month << "/" << animals[i].dateOfArrival.year << endl;
+    cout << "Date of arrival: " << dogs[i].dateOfArrival.day << "/" << dogs[i].dateOfArrival.month << "/" << dogs[i].dateOfArrival.year << endl;
 }
 
 void showAdoptedDog(DOG* adoptedDogs, int i, string gender, string condition)
@@ -80,9 +81,162 @@ void showAdoptedDog(DOG* adoptedDogs, int i, string gender, string condition)
     cout << "Date of adoption: " << adoptedDogs[i].dateOfAdoption.day << "/" << adoptedDogs[i].dateOfAdoption.month << "/" << adoptedDogs[i].dateOfAdoption.year << endl;
 }
 
+void showDogsInShelter(DOG* Dogs, int DogCount)
+{
+    string gender, condition;
+
+    for (int i = 0; i < DogCount; i++)
+    {
+        gender = genderEnumToString(Dogs, i);
+        condition = conditionEnumToString(Dogs, i);
+        showDog(Dogs, i, gender, condition);
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void showAdoptedDogs(DOG* adoptedDogs, int adoptedDogCount)
+{
+    string gender, condition;
+
+    for (int i = 0; i < adoptedDogCount; i++)
+    {
+        gender = genderEnumToString(adoptedDogs, i);
+        condition = conditionEnumToString(adoptedDogs, i);
+        showAdoptedDog(adoptedDogs, i, gender, condition);
+    }
+    cout << endl;
+}
+
+void showByBreed(DOG* Dogs, int DogCount, string breedUser)
+{
+    string gender, condition;
+
+    for (int i = 0; i < DogCount; i++)
+    {
+        gender = genderEnumToString(Dogs, i);
+        condition = conditionEnumToString(Dogs, i);
+        if (Dogs[i].breed == breedUser)
+        {
+            showDog(Dogs, i, gender, condition);
+        }
+    }
+    cout << endl;
+}
+
+void showByBreedMenu(DOG* Dogs, int DogCount)
+{
+    string breedUser;
+    cout << "Enter breed: ";
+    cin >> breedUser;
+    cout << endl;
+    showByBreed(Dogs, DogCount, breedUser);
+}
+
+
+void showById(DOG* dogs, int dogCount, int IdUser)
+{
+    string gender, condition;
+
+    for (int i = 0; i < dogCount; i++)
+    {
+        gender = genderEnumToString(dogs, i);
+        condition = conditionEnumToString(dogs, i);
+        if (dogs[i].id == IdUser)
+        {
+            showDog(dogs, i, gender, condition);
+        }
+    }
+    cout << endl;
+}
+
+void showByIdMenu(DOG* dogs, int dogCount)
+{
+    int IdUser;
+    cout << "Enter ID: ";
+    cin >> IdUser;
+    cout << endl;
+    showById(dogs, dogCount, IdUser);
+}
+
+void showBadConditionDogs(DOG* dogs, int dogCount)
+{
+    string gender, condition;
+
+    for (int i = 0; i < dogCount; i++)
+    {
+        gender = genderEnumToString(dogs, i);
+        condition = conditionEnumToString(dogs, i);
+
+        if (dogs[i].condition == condition::BAD)
+        {
+            showDog(dogs, i, gender, condition);
+        }
+    }
+    cout << endl;
+}
+
+void mainMenu(DOG* dogs, int dogCount, DOG* adoptedDogs, int adoptedDogCount)
+{
+    int choice;
+
+    cout << endl;
+    cout << "We rescued a new dog (add new dog to the registry:" << endl;
+    cout << "1. Add to the end of the list" << endl;
+    cout << "2. Add to a specific position on the list)" << endl;
+    cout << "3. An dog got adopted (remove dog from the list of current dogs and add to the adopted dogs list)" << endl;
+    cout << "4. Update an dog's profile" << endl;
+    cout << "5. Show all dogs in the shelter" << endl;
+    cout << "6. Show all dogs that have been adopted" << endl;
+    cout << "7. Show dog by ID number" << endl;
+    cout << "8. Show all dogs of specific species" << endl;
+    cout << "9. Show all dogs that need medical treatment" << endl;
+    cout << "10. EXIT" << endl;
+
+    cout << endl;
+
+    cout << "Choose an option: ";
+
+    cin >> choice;
+
+    cout << endl;
+
+    switch (choice)
+    {
+        /*case 1:addDog(); break;
+        case 2:insertDog(); break;
+        case 3:removeDog(); break;
+        case 4:updateDog(); break;*/
+    case 5:showDogsInShelter(dogs, dogCount); break;
+    case 6:showAdoptedDogs(adoptedDogs, adoptedDogCount); break;
+    case 7:showByIdMenu(dogs, dogCount); break;
+    case 8:showByBreedMenu(dogs, dogCount); break;
+    case 9:showBadConditionDogs(dogs, dogCount); break;
+    case 10: exit(0); break;
+    }
+}
 
 
 int main()
 {
-    cout << "ARTEMISSION" << endl;
+    int dogCount = 5;
+    int adoptedDogCount = 1;
+
+    DOG dogs[200] = {
+        {"Bark", 1, "Husky", 2,gender::MALE,condition::GOOD,{22, 3, 2021}},
+        {"Maya", 2, "Labrador", 1, gender::FEMALE, condition::GOOD, {18, 4, 2021}},
+        {"Corny", 3, "French bulldog", 3, gender::MALE, condition::BAD, {03, 7, 2021}},
+        {"Zeus", 4, "Golden retriever", 10, gender::MALE, condition::GOOD, {22, 9, 2020}},
+        {"Lilly", 5, "Pomeranian", 1, gender::FEMALE, condition::GOOD, {01, 4 , 2021}}
+    };
+
+    DOG adoptedDogs[200] = {
+        {"Sara", 13, "Golden retriever", 1, gender::FEMALE, condition::GOOD, {1,3,2020}}
+    };
+
+    cout << "Hello! We are team ARTEMISSION and welcome to our program! \nHow may we assist you todat?" << endl;
+
+
+    mainMenu(dogs, dogCount, adoptedDogs, adoptedDogCount);
 }
