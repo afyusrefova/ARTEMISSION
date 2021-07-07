def65cf1361be6ec4bf7eae37a85801597f430fd
afyusrefova <afyusrefova18@codingburgas.bg>
2021-07-07 10:12:19 +0300

    Add showDog and showAdoptedDog functions

--- Project ARTEMISsion/Project ARTEMISsion.cpp
+++ Project ARTEMISsion/Project ARTEMISsion.cpp
@@ -43,6 +43,44 @@ string genderEnumToString(DOG* dogs, int i)
     return gender;
 }
 
+string conditionEnumToString(DOG* dogs, int i)
+{
+    string condition;
+    if (dogs[i].condition == 0)
+    {
+        condition = "Good";
+    }
+    else if (dogs[i].condition == 1)
+    {
+        condition = "Bad";
+    }
+    return condition;
+}
+
+void showDog(DOG* dogs, int i, string gender, string condition)
+{
+    cout << "Name: " << animals[i].name << endl;
+    cout << "ID: " << animals[i].id << endl;
+    cout << "Breed: " << animals[i].breed << endl;
+    cout << "Age (in human years): " << animals[i].age << endl;
+    cout << "Gender: " << gender << endl;
+    cout << "Condition: " << condition << endl;
+    cout << "Date of arrival: " << animals[i].dateOfArrival.day << "/" << animals[i].dateOfArrival.month << "/" << animals[i].dateOfArrival.year << endl;
+}
+
+void showAdoptedDog(DOG* adoptedDogs, int i, string gender, string condition)
+{
+    cout << "Name: " << adoptedDogs[i].name << endl;
+    cout << "ID: " << adoptedDogs[i].id << endl;
+    cout << "Breed: " << adoptedDogs[i].breed << endl;
+    cout << "Age (in human years): " << adoptedDogs[i].age << endl;
+    cout << "Gender: " << gender << endl;
+    cout << "Condition: " << condition << endl;
+    cout << "Date of arrival: " << adoptedDogs[i].dateOfArrival.day << "/" << adoptedDogs[i].dateOfArrival.month << "/" << adoptedDogs[i].dateOfArrival.year << endl;
+    cout << "Date of adoption: " << adoptedDogs[i].dateOfAdoption.day << "/" << adoptedDogs[i].dateOfAdoption.month << "/" << adoptedDogs[i].dateOfAdoption.year << endl;
+}
+
+
 
 int main()
 {
